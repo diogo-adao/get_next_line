d0d31b9dd1cc37f6b80d4a7514ae7e5c58cc79ae
diogo-adao <diogoadao01x@gmail.com>
2024-05-09 19:31:41 +0100

    started gnl

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: diolivei <diolivei@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/09 15:59:56 by diolivei          #+#    #+#             */
+/*   Updated: 2024/05/09 16:52:38 by diolivei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+
+char	*get_next_line(int fd);
+
+#endif
